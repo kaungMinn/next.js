ccc202766aa4a4ac9ec78564239b02ccb38d6666
Tim Neutkens <tim@timneutkens.nl>
2025-08-26 16:03:58 +0200

    Revert

--- crates/next-core/src/app_page_loader_tree.rs
+++ crates/next-core/src/app_page_loader_tree.rs
@@ -190,9 +190,9 @@ impl AppPageLoaderTreeBuilder {
                 let identifier = magic_identifier::mangle(&format!("{name} #{i}"));
                 let inner_module_id = format!("METADATA_{i}");
 
-                self.base.imports.push(
-                    format!("const {identifier} = () => import(\"{inner_module_id}\");").into(),
-                );
+                self.base
+                    .imports
+                    .push(format!("import {identifier} from \"{inner_module_id}\";").into());
 
                 let source = dynamic_image_metadata_source(
                     *ResolvedVc::upcast(self.base.module_asset_context),
@@ -236,7 +236,7 @@ impl AppPageLoaderTreeBuilder {
 
         self.base
             .imports
-            .push(format!("const {identifier} = () => import(\"{inner_module_id}\");").into());
+            .push(format!("import {identifier} from \"{inner_module_id}\";").into());
         let module = Vc::upcast(StructuredImageModuleType::create_module(
             Vc::upcast(FileSource::new(path.clone())),
             BlurPlaceholderMode::None,
@@ -288,7 +288,7 @@ impl AppPageLoaderTreeBuilder {
 
             self.base
                 .imports
-                .push(format!("const {identifier} = () => import(\"{inner_module_id}\");").into());
+                .push(format!("import {identifier} from \"{inner_module_id}\";").into());
 
             let module = self
                 .base
